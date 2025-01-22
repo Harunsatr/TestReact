fb7523e7941a01935ea8dd72ab149c4299432e86
Harunsatr <harunsatr36@gmail.com>
2025-01-22 23:51:29 +0700

    fix4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar";
+import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 import Beranda from "./components/Beranda";
 import Berita from "./components/Berita";
